e48e318d0277c3ea9166a5afba414dc64f48119f
drvalle1 <drvalle1@users.noreply.github.com>
2020-05-28 10:25:29 -0400

    Commit 3
    Made changes so that users can provide initial values for break-points, if they want.

--- gibbs sampler main function.R
+++ gibbs sampler main function.R
@@ -1,15 +1,15 @@
-time.segm.behavior=function(dat=dat,ngibbs=ngibbs){
+time.segm.behavior=function(dat,ngibbs,breakpt=NULL){
   #useful stuff
   max.time=nrow(dat)
   ncat.data=apply(dat,2,max)
   
   #starting values
-  breakpt=floor(max.time/2)
+  if (is.null(breakpt)) breakpt=floor(max.time/2)
   
   for (i in 1:ngibbs){
     print(i)
     breakpt=samp.move(breakpt=breakpt,max.time=max.time,dat=dat,
-                      alpha=alpha,ncat.data=ncat.data,ndata.types=ndata.types)   
+                      alpha=alpha,ncat.data=ncat.data,ndata.types=ndata.types)
   }
   breakpt
 }
